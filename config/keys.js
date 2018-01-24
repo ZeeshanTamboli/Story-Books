3cd50d7a2b3842de5b878cd04647b610e7699800
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-24 22:20:43 +0530

    Created our google OAuth credentials

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://zeeshan:zeeshan@ds213338.mlab.com:13338/storybooks-dev',
+  googleClientID: '609344794779-b99pv74s5autfko8rgvb3m5d27crk64h.apps.googleusercontent.com',
+  googleClientSecret: '1AtBUX9hO4JGxNYYwYBFdI1F'
+}
