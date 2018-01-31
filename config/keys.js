68289a527ca87ef3ca6d709bed32f7d1cf086d0d
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-31 11:57:14 +0530

    Production keys variable

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://zeeshan:zeeshan@ds213338.mlab.com:13338/storybooks-dev',
-  googleClientID: '609344794779-b99pv74s5autfko8rgvb3m5d27crk64h.apps.googleusercontent.com',
-  googleClientSecret: '1AtBUX9hO4JGxNYYwYBFdI1F'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
